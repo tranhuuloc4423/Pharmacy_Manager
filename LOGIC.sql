5ef0027ae7b79592169782fa0c4d6f937cdfa650
Rokudev <91060671+tranhuuloc4423@users.noreply.github.com>
2024-03-14 09:00:41 +0700

    Update procedures LOGIC.sql

--- LOGIC.sql
+++ LOGIC.sql
@@ -147,3 +147,151 @@ END;
 
 /*EXEC XemChiTietHoaDon 4
 SELECT * FROM HoaDon*/
+
+-- 
+-- Tạo stored procedure thêm thuốc
+CREATE PROCEDURE [dbo].[ThemThuoc]
+    @MaLoaiThuoc INT,
+    @TenThuoc NVARCHAR(50),
+    @DonViTinh NVARCHAR(15),
+    @GiaBan DECIMAL(18,2)
+AS
+BEGIN
+    INSERT INTO [dbo].[Thuoc] (MaLoaiThuoc, TenThuoc, DonViTinh, GiaBan)
+    VALUES (@MaLoaiThuoc, @TenThuoc, @DonViTinh, @GiaBan);
+END
+GO
+
+-- Tạo stored procedure sửa thông tin thuốc
+CREATE PROCEDURE [dbo].[SuaThuoc]
+    @MaThuoc INT,
+    @MaLoaiThuoc INT,
+    @TenThuoc NVARCHAR(50),
+    @DonViTinh NVARCHAR(15),
+    @GiaBan DECIMAL(18,2)
+AS
+BEGIN
+    UPDATE [dbo].[Thuoc]
+    SET MaLoaiThuoc = @MaLoaiThuoc,
+        TenThuoc = @TenThuoc,
+        DonViTinh = @DonViTinh,
+        GiaBan = @GiaBan
+    WHERE MaThuoc = @MaThuoc;
+END
+GO
+
+-- Tạo stored procedure xóa thuốc
+CREATE PROCEDURE [dbo].[XoaThuoc]
+    @MaThuoc INT
+AS
+BEGIN
+    DELETE FROM [dbo].[Thuoc]
+    WHERE MaThuoc = @MaThuoc;
+END
+GO
+
+-- Tạo stored procedure thêm nhà cung cấp
+CREATE PROCEDURE [dbo].[ThemNhaCungCap]
+    @TenNhaCungCap NVARCHAR(50),
+    @DiaChi NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO [dbo].[NhaCungCap] (TenNhaCungCap, DiaChi)
+    VALUES (@TenNhaCungCap, @DiaChi);
+END
+GO
+
+-- Tạo stored procedure sửa thông tin nhà cung cấp
+CREATE PROCEDURE [dbo].[SuaNhaCungCap]
+    @MaNhaCungCap INT,
+    @TenNhaCungCap NVARCHAR(50),
+    @DiaChi NVARCHAR(50)
+AS
+BEGIN
+    UPDATE [dbo].[NhaCungCap]
+    SET TenNhaCungCap = @TenNhaCungCap,
+        DiaChi = @DiaChi
+    WHERE MaNhaCungCap = @MaNhaCungCap;
+END
+GO
+
+-- Tạo stored procedure xóa nhà cung cấp
+CREATE PROCEDURE [dbo].[XoaNhaCungCap]
+    @MaNhaCungCap INT
+AS
+BEGIN
+    DELETE FROM [dbo].[NhaCungCap]
+    WHERE MaNhaCungCap = @MaNhaCungCap;
+END
+GO
+
+-- Tạo stored procedure thêm phân loại
+CREATE PROCEDURE [dbo].[ThemPhanLoai]
+    @TenLoaiThuoc NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO [dbo].[PhanLoai] (TenLoaiThuoc)
+    VALUES (@TenLoaiThuoc);
+END
+GO
+
+-- Tạo stored procedure sửa thông tin phân loại
+CREATE PROCEDURE [dbo].[SuaPhanLoai]
+    @MaLoaiThuoc INT,
+    @TenLoaiThuoc NVARCHAR(50)
+AS
+BEGIN
+    UPDATE [dbo].[PhanLoai]
+    SET TenLoaiThuoc = @TenLoaiThuoc
+    WHERE MaLoaiThuoc = @MaLoaiThuoc;
+END
+GO
+
+-- Tạo stored procedure xóa phân loại
+CREATE PROCEDURE [dbo].[XoaPhanLoai]
+    @MaLoaiThuoc INT
+AS
+BEGIN
+    DELETE FROM [dbo].[PhanLoai]
+    WHERE MaLoaiThuoc = @MaLoaiThuoc;
+END
+GO
+
+-- Tạo stored procedure thêm kho thuốc
+CREATE PROCEDURE [dbo].[ThemKhoThuoc]
+    @MaThuoc INT,
+    @MaNhaCungCap INT,
+    @SoLuong INT,
+    @NgaySanXuat DATE,
+    @NgayHetHan DATE
+AS
+BEGIN
+    INSERT INTO [dbo].[KhoThuoc] (MaThuoc, MaNhaCungCap, SoLuong, NgaySanXuat, NgayHetHan)
+    VALUES (@MaThuoc, @MaNhaCungCap, @SoLuong, @NgaySanXuat, @NgayHetHan);
+END
+GO
+CREATE PROCEDURE [dbo].[SuaKhoThuoc]
+    @MaKhoThuoc INT,
+    @MaThuoc INT,
+    @MaNhaCungCap INT,
+    @SoLuong INT,
+    @NgaySanXuat DATE,
+    @NgayHetHan DATE
+AS
+BEGIN
+    UPDATE [dbo].[KhoThuoc]
+    SET MaThuoc = @MaThuoc,
+        MaNhaCungCap = @MaNhaCungCap,
+        SoLuong = @SoLuong,
+        NgaySanXuat = @NgaySanXuat,
+        NgayHetHan = @NgayHetHan
+    WHERE MaKhoThuoc = @MaKhoThuoc;
+END
+GO
+CREATE PROCEDURE [dbo].[XoaKhoThuoc]
+    @MaKhoThuoc INT
+AS
+BEGIN
+    DELETE FROM [dbo].[KhoThuoc]
+    WHERE MaKhoThuoc = @MaKhoThuoc;
+END
