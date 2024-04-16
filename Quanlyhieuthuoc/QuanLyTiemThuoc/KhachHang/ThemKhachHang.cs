41590e3135ab8ea88551e64814fcafeb07c7b109
Rokudev <91060671+tranhuuloc4423@users.noreply.github.com>
2024-04-16 08:32:14 +0700

    Update ThemKhachHang.cs

--- Quanlyhieuthuoc/QuanLyTiemThuoc/KhachHang/ThemKhachHang.cs
+++ Quanlyhieuthuoc/QuanLyTiemThuoc/KhachHang/ThemKhachHang.cs
@@ -26,15 +26,37 @@ namespace Quanlyhieuthuoc.KhachHang
         {
             string hoTen = txtHoTen.Text.Trim();
             string sdt = txtSoDienThoai.Text.Trim();
-            string khtt = txtKHTT.Text.Trim();
+            if(string.IsNullOrEmpty(hoTen))
+            {
+                MessageBox.Show("Vui lòng nhập họ tên khách hàng!");
+                return;
+            }
 
+            if(string.IsNullOrEmpty(sdt))
+            {
+                MessageBox.Show("Vui lòng nhập Số điện thoại khách hàng!");
+                return;
+            }
+
+            KhachHangEntity entity = new KhachHangEntity();
+            entity.hoTen = hoTen;
+            entity.SoDienThoai = sdt;
+            
             try
             {
-
+                var result = manager.ThemKhachHang(entity, ref error);
+                if(result) 
+                {
+                    MessageBox.Show("Thêm khách hàng thành công!");
+                    this.Close();
+                } else 
+                {
+                    MessageBox.Show("Thêm khách hàng không thành công!");
+                    MessageBox.Show("Lỗi : " + error);
+                }
             }
             catch (Exception ex)
             {
-
                 throw;
             }
         }
